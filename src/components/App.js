f0c25e69f243241b475158e047eb5ed1ab1eb890
Tenshilupus <Angel__CM@hotmail.com>
2023-01-23 19:58:03 +0100

    logic done

--- src/components/App.js
+++ src/components/App.js
@@ -26,11 +26,15 @@ import i7 from '../assets/i7.jpg';
 
 const images = [i1,i2,i3,i4,i5,i6,i7]
 
-const Loading = () => (
+//Alrenatively we can pass the prop directly to the component
+const Loading = ({calculatedwidth}) => (
     <aside>
         <div className="loading-bar">
             <label htmlFor="images-loaded" >Loading all images</label>
-            <progress id="images-loaded" max="100" value="50"></progress>
+            <progress 
+            id="images-loaded" 
+            max="100" 
+            value={calculatedwidth}></progress>
         </div>
     </aside>
 );
@@ -60,21 +64,32 @@ const App = () => {
         setNumLoaded((numLoaded) => numLoaded + 1);
     }
 
+    //Syntax declaration is defined first before the jsx is returned as html elements and rendered to the screen
     return (
-    <section> 
+        <section> 
         <header>
             <h1>Zesty</h1>
             <h2>A photography project <br/> by Ella Fieldling</h2>
             
-            <button onClick={null}>Click here</button>
-
-            <input type="text" name="example" autoComplete="off" onChange={handleChange} />
         </header>
 
         <figure>
-            <Loading calculatewidth={(numLoaded / images.length) * 100}/>
+            {numLoaded < images.length && <Loading calculatedwidth={(numLoaded / images.length) * 100}/>}
+            
+
             <figcaption>{currentImage + 1} / {images.length}</figcaption>
-            <img alt="" src={images[currentImage]} onClick={handleClick} onLoad={handleImageLoad}/>
+            {images.map((imageUrl,index) => (
+                <img 
+                alt=""
+                key={imageUrl} 
+                src={imageUrl} 
+                onClick={handleClick} 
+                onLoad={handleImageLoad}
+                //style={{opacity: currentImage === index ? 1 : 0}}
+                className={currentImage === index ? "display" : "hide"}
+                />
+            ))}
+
         </figure>
 
     </section>
